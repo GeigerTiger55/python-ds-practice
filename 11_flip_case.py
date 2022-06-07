24ae197637787491038ca9766d30e5646c8b4c06
GeigerTiger55 <elise.geiger@gmail.com>
2022-06-06 17:50:05 -0700

    completed 11-15

--- 11_flip_case.py
+++ 11_flip_case.py
@@ -11,4 +11,18 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+    swapped_phrase = ''
+
+    for char in phrase:
+        if char == to_swap.lower():
+            replace_char = char.upper()
+            swapped_phrase = (f"{swapped_phrase}{replace_char}")
+        elif char == to_swap.upper():
+            replace_char = char.lower()
+            swapped_phrase = (f"{swapped_phrase}{replace_char}")
+        else:
+            swapped_phrase = (f"{swapped_phrase}{char}")
+    return swapped_phrase
+
+    
 
